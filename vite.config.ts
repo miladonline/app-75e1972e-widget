18ee7efdc73564cf389bc4ba942aa5691b4f3bbf
miladonline <89708263+miladonline@users.noreply.github.com>
2025-10-24 15:57:17 -0700

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'widget.js',
+        assetFileNames: '[name].[ext]',
+        inlineDynamicImports: true
+      }
+    },
+    cssCodeSplit: false
+  }
+});
